48c13f3817f7caf8b86d3ae5dfb609e65b5f76c4
Sagar Verma <call2sagarverma@gmail.com>
2016-03-27 03:39:39 +0530

    Image Classifier Done

---
+++ codes/my_classifier.py
@@ -0,0 +1,48 @@
+
+import tensorflow.python.platform
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+import csv
+
+def create_graph():
+	f = gfile.FastGFile('classify_image_graph_def.pb', 'rb')
+	graph_def = tf.GraphDef()
+	graph_def.ParseFromString(f.read())
+	_ = tf.import_graph_def(graph_def, name='')
+
+def get_classes():
+	r1 = csv.reader(open("class_id_to_node_id.txt","rb"))
+	r2 = csv.reader(open("node_id_to_class.txt","rb"), delimiter="\t")
+
+	class_map = {}
+	label_map = {}
+
+	for row in r1:
+		class_map[int(row[0])] = row[1]
+
+	for row in r2:
+		label_map[row[0]] = row[1]
+
+	return class_map, label_map
+		
+def run_inference_on_image(image):
+	image_data = gfile.FastGFile(image, 'rb').read()
+ 
+	class_map, label_map = get_classes()
+
+	with tf.Session() as sess:
+		softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+		predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+		predictions = np.squeeze(predictions)
+		top_k = predictions.argsort()[-5:][::-1]
+		for k in top_k:
+			print label_map[class_map[k]]   
+
+def main(_):
+	create_graph()
+	image = "cropped_panda.jpg"
+	run_inference_on_image(image)
+
+if __name__ == '__main__':
+	tf.app.run()
--- server/server.py
+++ server/server.py
@@ -6,15 +6,54 @@ from flask import Flask, request, jsonify, render_template, url_for, redirect
 from flask_restful import Resource, Api
 import json, urllib, urllib2, unirest, re
 from urllib2 import URLError
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+import csv
 
 app = Flask(__name__)
+def create_graph():
+	f = gfile.FastGFile('../codes/classify_image_graph_def.pb', 'rb')
+	graph_def = tf.GraphDef()
+	graph_def.ParseFromString(f.read())
+	_ = tf.import_graph_def(graph_def, name='')
 
+create_graph()
 
+def get_classes():
+	r1 = csv.reader(open("../codes/class_id_to_node_id.txt","rb"))
+	r2 = csv.reader(open("../codes/node_id_to_class.txt","rb"), delimiter="\t")
+
+	class_map = {}
+	label_map = {}
+
+	for row in r1:
+		class_map[int(row[0])] = row[1]
+
+	for row in r2:
+		label_map[row[0]] = row[1]
+
+	return class_map, label_map
+
+class_map,label_map = get_classes()
+
+sess = tf.Session()
+		
+def get_predictions(image):	
+	image_data = gfile.FastGFile(image, 'rb').read()
+	softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')	
+	predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+	predictions = np.squeeze(predictions)
+	top_k = predictions.argsort()[-5:][::-1]
+	res = []
+	for k in top_k:
+		res.append(label_map[class_map[k]])
+	return res
 
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['text']
-
+    print get_predictions("../codes/cropped_panda.jpg")
    	
     return jsonify({'result':[]})
 
@@ -23,5 +62,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
